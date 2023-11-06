cf87831bcf737cad22859a897ca164bf9e153494
Beatriz Teixeira <149814751+CYONBEATRIZ@users.noreply.github.com>
2023-11-06 18:10:57 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+console.log("it´s working");
+
+$(".content01").hide();
+$(".content02").hide();
+$(".content03").hide();
+$(".content04").hide();
+$(".content05").hide();
+$(".content06").hide();
+
+
+
+$(".proj01").on("click",function(){
+$(".content01").toggle("show");
+});
+
+$(".proj02").on("click",function(){
+    $(".content02").toggle("show");
+    });
+
+$(".proj03").on("click",function(){
+$(".content03").toggle("show");
+});
+
+$(".proj04").on("click",function(){
+    $(".content04").toggle("show");
+    });
+
+$(".proj05").on("click",function(){
+$(".content05").toggle("show");
+});
+
+$(".proj06").on("click",function(){
+    $(".content06").toggle("show");
+    });
+
+
+ $(".up").on("click",function(){
+
+$(".content01").hide();
+$(".content02").hide();
+$(".content03").hide();
+$(".content04").hide();
+$(".content05").hide();
+$(".content06").hide();
+ });
+//*$(".po1").on("click",function(){
+    //$(".content01").toggle("show");
+   // $(".co1").toggle("show");
+    //});
+//__________________________________________________________ carrega no h3 e aparece os "p", carrega no h2 e 
+
+
